118d090605cfcc57db4a3d060d5f00960426c193
damien <damienqtpham@gmail.com>
2022-10-12 23:14:31 +1100

    added metadata

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,13 +45,13 @@ then
       # make RELEASE.yml context file
       echo "GIT_RELEASE:" >> RELEASE.yml
       echo "$(jq -M -r '.event' $INPUT_GITHUB_CONTEXT)" >> RELEASE.yml
-      echo "  ref_name: $(jq -M -r '.ref_name' INPUT_GITHUB_CONTEXT.json)" >> RELEASE.yml
-      echo "  published_at: $(jq -M -r '.event.release.published_at' INPUT_GITHUB_CONTEXT.json)" >> RELEASE.yml
-      echo "  target_commitish: $(jq -M -r '.event.release.target_commitish' INPUT_GITHUB_CONTEXT.json)" >> RELEASE.yml
-      echo "  triggering_actor: $(jq -M -r '.triggering_actor' INPUT_GITHUB_CONTEXT.json)" >> RELEASE.yml
-      echo "  title: $(jq -M -r '.event.release.name' INPUT_GITHUB_CONTEXT.json)" >> RELEASE.yml
-      echo "  description: $(jq -M -r '.event.release.body' INPUT_GITHUB_CONTEXT.json)" >> RELEASE.yml
-      echo "  url: $(jq -M -r '.event.release.html_url' INPUT_GITHUB_CONTEXT.json)" >> RELEASE.yml
+      #echo "  ref_name: $(jq -M -r '.ref_name' INPUT_GITHUB_CONTEXT.json)" >> RELEASE.yml
+      #echo "  published_at: $(jq -M -r '.event.release.published_at' INPUT_GITHUB_CONTEXT.json)" >> RELEASE.yml
+      #echo "  target_commitish: $(jq -M -r '.event.release.target_commitish' INPUT_GITHUB_CONTEXT.json)" >> RELEASE.yml
+      #echo "  triggering_actor: $(jq -M -r '.triggering_actor' INPUT_GITHUB_CONTEXT.json)" >> RELEASE.yml
+      #echo "  title: $(jq -M -r '.event.release.name' INPUT_GITHUB_CONTEXT.json)" >> RELEASE.yml
+      #echo "  description: $(jq -M -r '.event.release.body' INPUT_GITHUB_CONTEXT.json)" >> RELEASE.yml
+      #echo "  url: $(jq -M -r '.event.release.html_url' INPUT_GITHUB_CONTEXT.json)" >> RELEASE.yml
 
       # grab the deploy.zip file and compress again into release.zip file containing deploy.zip, install.sh, README*
       zip -r $INPUT_FILENAME $INPUT_PATH --include $INPUT_INCLUSIONS || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
